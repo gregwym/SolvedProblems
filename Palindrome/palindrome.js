94bd3060d8789c6accb9bdd277c1782d63fed2ae
Greg Wang <gregwym@gmail.com>
2015-04-05 18:57:27 -0400

    Palindrome.

---
+++ Palindrome/palindrome.js
@@ -0,0 +1,45 @@
+/*
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+For example,
+"A man, a plan, a canal: Panama" is a palindrome.
+"race a car" is not a palindrome.
+
+Note:
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+For the purpose of this problem, we define empty string as valid palindrome.
+*/
+
+/**
+ * @param {string} str
+ * @return {boolean}
+ */
+
+var isAlphanumeric = function(char) {
+    return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9');
+};
+
+var isPalindrome = function(str) {
+    var i = 0;
+    var j = str.length - 1;
+    var left, rigth;
+    while(i < j) {
+        left = str.charAt(i);
+        right = str.charAt(j);
+        if (!isAlphanumeric(left)) {
+            i++;
+            continue;
+        }
+        if (!isAlphanumeric(right)) {
+            j--;
+            continue;
+        }
+
+        if (left.toLowerCase() !== right.toLowerCase()) return false;
+        i++;
+        j--;
+    }
+
+    return true;
+};
