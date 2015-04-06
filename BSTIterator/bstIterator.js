cad090cfd386b4e51c17ea219e00d7298e4ff298
Greg Wang <gregwym@gmail.com>
2015-04-05 20:19:21 -0400

    BST Iterator.

---
+++ BSTIterator/bstIterator.js
@@ -0,0 +1,61 @@
+/*
+https://leetcode.com/problems/binary-search-tree-iterator/
+
+Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+
+Calling next() will return the next smallest number in the BST.
+
+Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+
+Credits:
+Special thanks to @ts for adding this problem and creating all test cases.
+*/
+
+/**
+ * Definition for binary tree
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @constructor
+ * @param {TreeNode} root - root of the binary search tree
+ */
+var BSTIterator = function(root) {
+    this.stack = [];
+    while(root) {
+        this.stack.push(root);
+        root = root.left;
+    }
+};
+
+
+/**
+ * @this BSTIterator
+ * @returns {boolean} - whether we have a next smallest number
+ */
+BSTIterator.prototype.hasNext = function() {
+    return !!this.stack.length;
+};
+
+/**
+ * @this BSTIterator
+ * @returns {number} - the next smallest number
+ */
+BSTIterator.prototype.next = function() {
+    var next = this.stack.pop();
+    var bigger = next.right;
+    while(bigger) {
+        this.stack.push(bigger);
+        bigger = bigger.left;
+    }
+    return next.val;
+};
+
+/**
+ * Your BSTIterator will be called like this:
+ * var i = new BSTIterator(root), a = [];
+ * while (i.hasNext()) a.push(i.next());
+*/
